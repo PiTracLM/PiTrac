273478384811d23390c1ee12465ed7a4e56c617b
Connor Gallopo <connor.gallopo@me.com>
2026-03-23 16:12:19 -0400

    add libncnn-dev to APT dependencies, improve test environment script

--- packaging/src/lib/pitrac-common-functions.sh
+++ packaging/src/lib/pitrac-common-functions.sh
@@ -849,6 +849,7 @@ install_dependencies_from_apt() {
         "libactivemq-cpp-dev"     # ActiveMQ C++ client headers
         "libopencv4.11"           # OpenCV runtime (Pi5-optimized build)
         "libopencv-dev"           # OpenCV development headers
+        "libncnn-dev"             # ncnn inference framework (static lib + headers)
         "libonnxruntime1.17.3"    # ONNX Runtime with XNNPACK (1.22.x has Pi5 issues)
     )
 
