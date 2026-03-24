4933e2c8a22c6f9299da4cea81ccba6fee9d6091
Connor Gallopo <connor.gallopo@me.com>
2026-03-23 21:26:40 -0400

    update model defaults to 640x640 input, raise confidence to 0.65

--- Software/LMSourceCode/ImageProcessing/ball_image_proc.cpp
+++ Software/LMSourceCode/ImageProcessing/ball_image_proc.cpp
@@ -209,9 +209,9 @@ namespace golf_sim {
     #else
     std::string BallImageProc::kModelPath = "../ml_models/yolo26-ball-detector";
     #endif
-    float BallImageProc::kModelConfidenceThreshold = 0.5f;
+    float BallImageProc::kModelConfidenceThreshold = 0.65f;
     float BallImageProc::kModelNMSThreshold = 0.4f;
-    int BallImageProc::kModelInputSize = 1472;
+    int BallImageProc::kModelInputSize = 640;
     std::string BallImageProc::kModelDeviceType = "CPU";
 
     std::string BallImageProc::kInferenceBackend = "ncnn";
