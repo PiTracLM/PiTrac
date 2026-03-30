81d14d19950d6378d106641d0ee67a935742cfff
Connor Gallopo <connor.gallopo@me.com>
2026-03-30 18:08:41 -0400

    add shot-result HTTP endpoint, remove ActiveMQ from web server

--- Software/web-server/constants.py
+++ Software/web-server/constants.py
@@ -5,10 +5,6 @@ from pathlib import Path
 MPS_TO_MPH = 2.237
 EXPECTED_DATA_LENGTH = 12
 
-DEFAULT_BROKER = "tcp://localhost:61616"
-STOMP_PORT = 61613
-DEFAULT_USERNAME = "admin"
-DEFAULT_PASSWORD = "admin"
 
 HOME_DIR = Path.home()
 PITRAC_DIR = HOME_DIR / ".pitrac"
